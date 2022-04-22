dec362a168e6891ef364ce322b13e3da15f0bfc3
Stephen Robbins <swrobbin@gmail.com>
2022-04-22 13:28:58 -0400

    Added DOM element functionality

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if(!isValidHex(hex)) return;
+    
+    const strippedHex = hex.replace("#", '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
 const isValidHex = (hex) => {
-
-if(!hex) return false;
-
-const strippedHex = hex.replace("#", '');
-return strippedHex.length === 3 || strippedHex.length === 6;
-
-
-
-
+    if(!hex) return false;
+    
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
 }
