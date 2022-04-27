848e22f7ba017732294a474dc1731755c9f07871
Stephen Robbins <swrobbin@gmail.com>
2022-04-27 15:09:23 -0400

    Added JS convert Hex to RGB value

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ const isValidHex = (hex) => {
     const strippedHex = hex.replace('#', '');
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
+
+const convertHexToRGB = (hex) => {
+
+if(!isValidHex(hex)) return null;
+
+let strippedHex = hex.replace("#", '');
+
+if(strippedHex.length === 3){
+    strippedHex = strippedHex[0] + strippedHex[0]
+    + strippedHex[1] + strippedHex[1]
+    + strippedHex[2] + strippedHex[2]
+}
+const r = parseInt(strippedHex.substring(0, 2), 16);
+const g = parseInt(strippedHex.substring(2, 4), 16);
+const b = parseInt(strippedHex.substring(4, 6), 16);
+
+return {r, g, b}
+}
+console.log(convertHexToRGB("ffe"))
