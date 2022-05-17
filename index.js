e7475420fd09225c69b4545f63b53a285e7f5c12
Stephen Robbins <swrobbin@gmail.com>
2022-05-17 11:11:43 -0400

    added alter color functionality

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
-const sliderText = document.getElementById("sliderText");
-const slider = document.getElementById("slider");
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
 
 hexInput.addEventListener('keyup', () => {
-    const hex = hexInput.value;
-    if(!isValidHex(hex)) return;
-    
-    const strippedHex = hex.replace("#", '');
-    inputColor.style.backgroundColor = "#" + strippedHex;
+  
+  const hex = hexInput.value;
+  if(!isValidHex(hex)) return;
+  
+  const strippedHex = hex.replace('#', '');
+
+  inputColor.style.backgroundColor = "#" + strippedHex;  
 })
 
 const isValidHex = (hex) => {
@@ -18,33 +22,65 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
+
+
 const convertHexToRGB = (hex) => {
+  if(!isValidHex(hex)) return null;
+  
+  let strippedHex = hex.replace('#', '');
 
-if(!isValidHex(hex)) return null;
-
-let strippedHex = hex.replace("#", '');
-
-if(strippedHex.length === 3){
-    strippedHex = strippedHex[0] + strippedHex[0]
-    + strippedHex[1] + strippedHex[1]
-    + strippedHex[2] + strippedHex[2]
+  if(strippedHex.length === 3){
+    strippedHex = strippedHex[0] + strippedHex[0] 
+    + strippedHex[1] + strippedHex[1] 
+    + strippedHex[2] + strippedHex[2];
+  }
+  
+  const r  = parseInt(strippedHex.substring(0,2), 16);
+  const g  = parseInt(strippedHex.substring(2,4), 16);
+  const b  = parseInt(strippedHex.substring(4,6), 16);
+  
+  return {r,g,b};
 }
-const r = parseInt(strippedHex.substring(0, 2), 16);
-const g = parseInt(strippedHex.substring(2, 4), 16);
-const b = parseInt(strippedHex.substring(4, 6), 16);
 
-return {r, g, b}
+const convertRGBToHex = (r,g,b) => {
+  const firstPair = ("0" + r.toString(16)).slice(-2);
+  const secondPair = ("0" + g.toString(16)).slice(-2);
+  const thirdPair = ("0" + b.toString(16)).slice(-2);
+  
+  const hex = "#" + firstPair + secondPair + thirdPair;
+  return hex;
 }
-// console.log(convertHexToRGB("ffe"))
 
-const convertRGBToHex = (r, g, b) => {
-    const firstPair = ("0" + r.toString(16)).slice(-2);
-    const secondPair = ("0" + g.toString(16)).slice(-2);
-    const thirdPair = ("0" + b.toString(16)).slice(-2);
+const alterColor = (hex, percentage) => {
+  const {r, g, b} = convertHexToRGB(hex);
+  const amount = Math.floor((percentage / 100) * 255);
+  const newR = increaseWithin0To255(r, amount);
+  const newG = increaseWithin0To255(g, amount);
+  const newB = increaseWithin0To255(b, amount)
 
-    const hex = "#" + firstPair + secondPair + thirdPair;
-    return hex;
+  return convertRGBToHex(newR, newG, newB);  
 }
-slider.addEventListener("input", () => {
-    sliderText.textContent = `${slider.value}%`
+
+
+const increaseWithin0To255 = (hex, amount) => {
+  const newHex = hex + amount;
+  if(newHex > 255) return 255;
+  if(newHex < 0) return 0;
+  return newHex;
+//   console.log("hex", hex, "amount", amount)
+//   return Math.min(255, Math.max(0, hex + amount));
+}
+
+// alterColor("fff", 10)
+
+slider.addEventListener('input', () => {
+  if(!isValidHex(hexInput.value)) return;
+  
+  sliderText.textContent = `${slider.value}%`;
+  
+  //get the altered hex value
+  const alteredHex = alterColor(hexInput.value, slider.value);
+  alteredColor.style.backgroundColor = alteredHex;
+  alteredColorText.innerText = `Altered Color ${alteredHex}`; 
+  //update the altered color
 })
